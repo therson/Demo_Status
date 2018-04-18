308995f00059b7955092a6e1885d8d67be693c29
Taylor Herson <therson@hortonworks.com>
2018-04-18 14:59:23 -0400

    first commit

---
+++ Demo_status.py
@@ -0,0 +1,63 @@
+import socket
+from contextlib import closing
+
+def check_socket(host, port):
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        if sock.connect_ex((host, port)) == 0:
+            print("Port is open")
+        else:
+            print("Port is not open")
+
+def river_status():
+	ambari_url 		= 'river0.field.hortonworks.com'
+	ambari_port 	= 8080
+	registry_url 	= 'river0.field.hortonworks.com'
+	registry_port 	= 7788
+	superset_url 	= 'river8.field.hortonworks.com'
+	superset_port 	= 9088
+	nifi_url 		= 'river8.field.hortonworks.com'
+	nifi_port 		= 9090
+	print("Checking River Ambari Port")
+	check_socket(ambari_url, ambari_port)
+	print("Checking River Registry Port")
+	check_socket(registry_url, registry_port)
+	print("Checking River Superset Port")
+	check_socket(superset_url, superset_port)
+	print("Checking River Nifi Port")
+	check_socket(nifi_url, nifi_port)
+
+def creditcard_status():
+	host_1 = 'creditcard-1.field.hortonworks.com'
+	host_2 = 'creditcard-2.field.hortonworks.com'
+	nifi_port 		= 9090
+	registry_port 	= 7788
+	ui_port 		= 8098
+	print("Checking CC NiFi Port")
+	check_socket(host_1, nifi_port)
+	print("Checking CC Registry Port")
+	check_socket(host_1, registry_port)
+	print("Checking UI Port")
+	check_socket(host_1, ui_port)
+
+def lake_status():
+	host_1 = 'lake1.field.hortonworks.com'
+	host_9 = 'lake9.field.hortonworks.com'
+	host_4 = 'lake4.field.hortonworks.com'
+	host_8 = 'lake8.field.hortonworks.com'
+	ambari_port		= 8080
+	nifi_port 		= 9090
+	registry_port 	= 7788
+	superset_port	= 9088
+	print("Checking Lake Ambari Status")
+	check_socket(host_1, ambari_port)
+	print("Checking Lake NiFi Status")
+	check_socket(host_9, nifi_port)
+	print("Checking Lake Registry Status")
+	check_socket(host_8, registry_port)
+	print("Checking Lake Superset Port")
+	check_socket(host_4, superset_port)
+
+if __name__ == "__main__":
+	river_status()
+	creditcard_status()
+	lake_status()
